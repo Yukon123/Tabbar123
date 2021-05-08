e5d1eda5b394104cd42f46b61c2b8b55b1bc3ad5
Yukon <zengyongkun@gmail.com>
2021-05-08 16:28:30 +0800

    tabbar

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="App">
+    <router-view> </router-view>
+    <maintabbar></maintabbar>
+  </div>
+</template>
+
+<script>
+import maintabbar from "./components/maintabbar";
+export default {
+  name: "App",
+  components: { maintabbar },
+};
+</script>
+
+<style>
+@import "./assets/css/basic";
+</style>
